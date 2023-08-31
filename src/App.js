79570bb161411e1170921bdddb80c3ba81f93a02
Tutku Ince <tutku.ince@outlook.com>
2023-08-31 21:26:59 +0300

    create table for todo

--- src/App.js
+++ src/App.js
@@ -3,14 +3,35 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>We made our first React App!</p>
-      </header>
+    <div>
+      <div>
+        <div>
+          <h2>Your Todos</h2>
+        </div>
+        <div>
+          <table border='1'>
+            <thead>
+              <tr>
+                <th scope='col'>#</th>
+                <th scope='col'>Description</th>
+                <th scope='col'>Assigned</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope='row'>1</th>
+                <td>Feed dog</td>
+                <td>Eric</td>
+              </tr>
+              <tr>
+                <th scope='col'>2</th>
+                <td>Get haircut</td>
+                <td>Eric</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 }
