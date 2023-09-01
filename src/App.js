64cde8393748e9379be4a1d2c82069c2f94afa63
Tutku Ince <tutku.ince@outlook.com>
2023-09-01 15:57:18 +0300

    react onClick listeners

--- src/App.js
+++ src/App.js
@@ -2,11 +2,24 @@ import './App.css';
 import TodoTable from './components/TodoTable';
 
 function App() {
-  const todos = [
+  let todos = [
     {rowNumber: 1, rowDescription: 'Feed puppy', rowAssigned: 'User One'},
     {rowNumber: 2, rowDescription: 'Water plants', rowAssigned: 'User Two'},
     {rowNumber: 3, rowDescription: 'Make dinner', rowAssigned: 'User One'}
   ];
+
+  const addTodo = () => {
+    if(todos.length > 0) {
+      const newTodo = {
+        rowNumber: todos.length + 1, 
+        rowDescription: 'New Todo',
+        rowAssigned: 'User Three'
+      };
+      todos.push(newTodo);
+      console.log(todos);
+    }
+  }
+
   return (
     <div className="mt-5 container">
       <div className="card">
@@ -15,6 +28,9 @@ function App() {
         </div>
         <div className="card-body">
           <TodoTable todos={todos}/>
+          <button className='btn btn-primary' onClick={addTodo}>
+            Add new Todo
+          </button>
         </div>
       </div>
     </div>
